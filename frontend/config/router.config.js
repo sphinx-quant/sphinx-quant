735bdf5a33db86a3aa887e0dd05adb981b3412a6
SphinxQuant <admin@sphinxquant.com>
2019-04-02 18:20:01 +0800

    增加策略编辑页面

--- frontend/config/router.config.js
+++ frontend/config/router.config.js
@@ -46,6 +46,10 @@ export default [
             name: 'portfolio',
             component: './ComingSoon',
           },
+          {
+            path: '/strategy/editor',
+            component: './Strategy/Editor',
+          },
         ],
       },
       {

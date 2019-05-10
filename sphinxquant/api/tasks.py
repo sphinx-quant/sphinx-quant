0ede8a79d0093c3a8a8ecf51af11d5058b4f5614
SphinxQuant <admin@sphinxquant.com>
2019-05-10 18:26:29 +0800

    使用black格式化/使用Enum

--- sphinxquant/api/tasks.py
+++ sphinxquant/api/tasks.py
@@ -1,20 +1,23 @@
 # Create your tasks here
 # from __future__ import absolute_import, unicode_literals
-import sys
 import os
+import sys
 import tempfile
-import time
+from contextlib import contextmanager
+from datetime import datetime
+
 from celery import shared_task
 from vnpy.app.cta_strategy.backtesting import BacktestingEngine
-from datetime import datetime
-from contextlib import contextmanager
-from .models import SourceCode, Strategy, Backtest, BACKTEST_STATUS_TYPE
+
+from .models import Backtest, SourceCode, Strategy
+from utils.const import BacktestStatusType
 
 
 @contextmanager
 def capture():
     import sys
     from io import StringIO
+
     oldout, olderr = sys.stdout, sys.stderr
     try:
         out = [StringIO(), StringIO()]
@@ -32,25 +35,37 @@ def add(x, y):
 
 
 @shared_task
-def backtest(strategy_id, code_text, class_name, vt_symbol, interval,
-             start_date, end_date, rate, slippage, size, pricetick, capital):
-    tmpf = tempfile.NamedTemporaryFile(suffix='.py', delete=True)
+def backtest(
+    strategy_id: str,
+    code_text: str,
+    class_name: str,
+    vt_symbol: str,
+    interval: str,
+    start_date: datetime,
+    end_date: datetime,
+    rate: float,
+    slippage: float,
+    size: float,
+    pricetick: float,
+    capital: float,
+):
+    tmpf = tempfile.NamedTemporaryFile(suffix=".py", delete=True)
     # 新建回测对象
     strategy = Strategy.objects.get(id=strategy_id)
     bt_obj = Backtest.objects.create(
         name=strategy.name,
         start_date=datetime.strptime(start_date, "%Y-%m-%dT%H:%M:%S"),
         end_date=datetime.strptime(end_date, "%Y-%m-%dT%H:%M:%S"),
-        status='P',
+        status=BacktestStatusType.PROCESS,
         total_profit_percent=0.0,
         year_profit_percent=0.0,
         max_dropdown_percent=0.0,
-        source_code=SourceCode.objects.create(code_text=code_text or ''),
+        source_code=SourceCode.objects.create(code_text=code_text or ""),
         strategy=strategy,
     )
     with capture() as out:
         try:
-            tmpf.write(code_text.encode('utf8'))
+            tmpf.write(code_text.encode("utf8"))
             tmpf.flush()
             tmpmodule_path, tmpmodule_file_name = os.path.split(tmpf.name)
             tmpmodule_name = tmpmodule_file_name[:-3]
@@ -75,14 +90,13 @@ def backtest(strategy_id, code_text, class_name, vt_symbol, interval,
             engine.load_data()
             engine.run_backtesting()
             df = engine.calculate_result()
-            print(df)
             engine.calculate_statistics()
             # 完成回测
-            bt_obj.status = 'D'
+            bt_obj.status = BacktestStatusType.DONE
         except Exception as e:
             print(e)
             # 中途发生错误
-            bt_obj.status = 'E'
+            bt_obj.status = BacktestStatusType.ERROR
         finally:
             tmpf.close()
     bt_obj.logs = out[0]
---
+++ sphinxquant/utils/const.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+
+def enum_to_choices(enum: Enum):
+    """ cover enum to choice """
+    return [(tag.name, tag.value) for tag in enum]
+
+
+class BacktestStatusType(Enum):
+    """ 回测状态 """
+
+    START = "START"
+    PROCESS = "PROCESS"
+    DONE = "DONE"
+    ERROR = "ERROR"
+    ABORT = "ABORT"
+
+
+class StrategyType(Enum):
+    """ 策略类型 """
+
+    STOCK = "STOCK"
+    FUTURES = "FUTURES"
+    CRYPTO_CURRENCY = "CRYPTO_CURRENCY"
+    OTHER = "OTHER"
+
+
+class BarType(Enum):
+    """ Bar类型 """
+
+    TICK = "TICK"
+    MINUTE = "MINUTE"
+    DAY = "DAY"
